1c502493c4b22ccc78bf00b663de322e69c12f9c
Joe <joerscout@gmail.com>
2021-03-14 23:08:36 -0400

    aded a check if zsh is installed

--- .dotfiles.sh
+++ .dotfiles.sh
@@ -16,33 +16,35 @@ if sudo -v | grep -i 'Sorry' ; then
 else
 	SUDOER='yes'
 fi
-if echo "$OS_LIKE" | grep -i 'arch' ; then
-	echo 'arch-based'
-	:wsudo pacman -S --noconfirm zsh vim wget curl xsel
-elif echo "$OS_LIKE" | grep -i 'debian' ; then
-	echo 'debian-based'
-	sudo apt update -y
-	sudo apt install -y zsh vim wget curl xsel
-elif echo "$OS_LIKE" | grep -i 'suse' ; then
-	echo 'suse-based'
-	sudo zypper refresh
-	sudo zypper install zsh vim wget curl xsel
-elif echo "$OS_LIKE" | grep -i 'fedora' ; then
-	echo 'fedora-based'
-	sudo dnf install zsh vim wget curl xsel
-elif sw_vers | grep -i 'mac os' ; then
-	echo 'FUCK this is mac' 
-else
-	echo 'OS not recognized :('
+if [ SUDOER == yes ] ; then
+	if echo "$OS_LIKE" | grep -i 'arch' ; then
+		echo 'arch-based'
+		sudo pacman -S --noconfirm zsh vim wget curl xsel
+	elif echo "$OS_LIKE" | grep -i 'debian' ; then
+		echo 'debian-based'
+		sudo apt update -y
+		sudo apt install -y zsh vim wget curl xsel
+	elif echo "$OS_LIKE" | grep -i 'suse' ; then
+		echo 'suse-based'
+		sudo zypper refresh
+		sudo zypper install zsh vim wget curl xsel
+	elif echo "$OS_LIKE" | grep -i 'fedora' ; then
+		echo 'fedora-based'
+		sudo dnf install zsh vim wget curl xsel
+	elif sw_vers | grep -i 'mac os' ; then
+		echo 'FUCK this is mac' 
+	else
+		echo 'OS not recognized :('
+	fi
 fi
 # change shell to zsh
 if echo "$SHELL" | grep -i 'zsh' ; then
 	echo 'shell already zsh'
-else
-	sudo chsh -s /bin/zsh $USER
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else 
+	chsh -s 'which zsh'
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
-# install oh-my-zsh without running zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 # install vim-plug
 curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # define git repo directory
